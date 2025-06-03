dea9bfd444c6550bb83ae79642b02fb759ca5609
Dustin Walker <me@dustinwalker.com>
2025-06-03 17:24:57 -0500

    ADD: Json endpoint

--- src/controllers/DefaultController.php
+++ src/controllers/DefaultController.php
@@ -12,14 +12,32 @@ namespace astuteo\astuteopulse\controllers;
 
 use astuteo\astuteopulse\services\BroadcastStatusService;
 
+
+use Craft;
 use craft\web\Controller;
+use craft\web\Response;
+
 
 class DefaultController extends Controller
 {
-    protected array|int|bool $allowAnonymous = ['index'];
+    protected array|int|bool $allowAnonymous = ['index', 'json'];
+
+    /**
+     * Handles the index action
+     * 
+     * @return \yii\web\Response
+     */
     public function actionIndex(): bool|string
     {
         return BroadcastStatusService::broadcastInfo();
+    }
+
+    public function actionJson(): \yii\web\Response
+    {
+        $response = Craft::$app->getResponse();
+        $response->format = \yii\web\Response::FORMAT_JSON;
+        $response->data = json_decode(BroadcastStatusService::broadcastInfo(), true);
+        return $response;
     }
 
 }
